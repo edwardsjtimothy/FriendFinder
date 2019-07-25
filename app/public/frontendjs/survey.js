36493776380fd6afc506efa61989d212977cfd67
Enragedtoast <edwardsjtimothy@gmail.com>
2019-07-25 18:33:26 -0500

    object accepted user input and checking to make sure all fields are filled

--- app/public/frontendjs/survey.js
+++ app/public/frontendjs/survey.js
@@ -4,43 +4,49 @@ $(function () {
     $("#submit").on("click", function (event) {
         event.preventDefault();
 
-
-       
-        $(".custom-select").each(function () {
+        $(".user-res").each(function () {
 
             if ($(this).val() === "") {
-            $(this).css("border", "1px solid red");
-            incomplete = true;
+                $(this).css("border", "1px solid red");
+                incomplete = true;
             } else if ($(this).val() !== "") {
-            $(this).css("border", "1px solid grey");
-            };
-
-            });
-
-            if ($("select").val() !== "") {
+                $(this).css("border", "1px solid grey");
                 incomplete = false;
             };
 
-            if (incomplete === false) {
+        });
+
+        if (incomplete === false) {
             var rankings = {
                 name: $("#name").val().trim(),
                 photo: $("#photo-link").val().trim(),
-                response1: $("#statement-one").val().trim(),
-                response2: $("#statement-two").val().trim(),
-                response3: $("#statement-three").val().trim(),
-                response4: $("#statement-four").val().trim(),
-                response5: $("#statement-five").val().trim(),
-                response6: $("#statement-six").val().trim(),
-                response7: $("#statement-seven").val().trim(),
-                response8: $("#statement-eight").val().trim(),
-                response9: $("#statement-nine").val().trim(),
-                response10: $("#statement-ten").val().trim(),
+                response1: parseInt($("#statement-one").val().trim()),
+                response2: parseInt($("#statement-two").val().trim()),
+                response3: parseInt($("#statement-three").val().trim()),
+                response4: parseInt($("#statement-four").val().trim()),
+                response5: parseInt($("#statement-five").val().trim()),
+                response6: parseInt($("#statement-six").val().trim()),
+                response7: parseInt($("#statement-seven").val().trim()),
+                response8: parseInt($("#statement-eight").val().trim()),
+                response9: parseInt($("#statement-nine").val().trim()),
+                response10: parseInt($("#statement-ten").val().trim()),
             };
         } else {
             alert("Your responses to these statements are crucial to determining you new best friend forever. Please provide a ranking for each statement.");
         };
 
-            console.log(rankings);
-    
+        console.log(rankings);
+
+
+        $.post("/data/friends", rankings,
+      function(data) {
+
+        // If a table is available... tell user they are booked.
+        if (data) {
+          alert("Yay! You are officially booked!");
+        }
+
+      });
+
     });
 });
