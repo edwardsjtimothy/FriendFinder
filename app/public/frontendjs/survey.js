ccd42b2e9b3940decb60e406bb3a8d48897a6108
Enragedtoast <edwardsjtimothy@gmail.com>
2019-07-26 12:21:45 -0500

    refactored click function that gathers survey data. Trying to get post to work

--- app/public/frontendjs/survey.js
+++ app/public/frontendjs/survey.js
@@ -4,6 +4,15 @@ $(function () {
     $("#submit").on("click", function (event) {
         event.preventDefault();
 
+        var rankings = {
+            name: $("#name").val().trim(),
+            photo: $("#photo-link").val().trim(),
+            scores: [],
+        };
+        $(".custom-select").each(function () {
+        rankings.scores.push(parseInt($(this).val().trim()));
+        });
+
         $(".user-res").each(function () {
 
             if ($(this).val() === "") {
@@ -16,37 +25,19 @@ $(function () {
 
         });
 
+    
         if (incomplete === false) {
-            var rankings = {
-                name: $("#name").val().trim(),
-                photo: $("#photo-link").val().trim(),
-                response1: parseInt($("#statement-one").val().trim()),
-                response2: parseInt($("#statement-two").val().trim()),
-                response3: parseInt($("#statement-three").val().trim()),
-                response4: parseInt($("#statement-four").val().trim()),
-                response5: parseInt($("#statement-five").val().trim()),
-                response6: parseInt($("#statement-six").val().trim()),
-                response7: parseInt($("#statement-seven").val().trim()),
-                response8: parseInt($("#statement-eight").val().trim()),
-                response9: parseInt($("#statement-nine").val().trim()),
-                response10: parseInt($("#statement-ten").val().trim()),
-            };
+            console.log("form complete");
+
+            $.post("/data/friends", rankings, function(data) {
+                
+                if (data)console.log(friends);
+                
+            });
+       
         } else {
             alert("Your responses to these statements are crucial to determining you new best friend forever. Please provide a ranking for each statement.");
         };
-
-        console.log(rankings);
-
-
-        $.post("/data/friends", rankings,
-      function(data) {
-
-        // If a table is available... tell user they are booked.
-        if (data) {
-          alert("Yay! You are officially booked!");
-        }
-
-      });
 
     });
 });
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,12 @@ var surveyData = require("../data/friends");
 
 module.exports = function(app) {
   
-  app.get("/friends", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(surveyData);
   });
 
  
-  app.post("/friends", function(req, res) {
+  app.post("/api/friends", function(req, res) {
     surveyData.push(req.body);
     
   });
